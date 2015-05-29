e39554a502691f5658973adbdd0422b1ebb6e034
martosv <virginia.martos.muoz@gmail.com>
2015-05-29 11:45:56 +0200

    Random movies

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,9 @@
 class QuestionsController < ApplicationController
+	def index
+		@random_movies = Question.random_movies
+		#@question_movie = 
+		#@question = @ramdom_questions.take_a_year
+	end
+
+
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,10 @@
 class Question < ActiveRecord::Base
+	def search
+		movies = Imdb::Search.new("love")
+	end
+
+	def take_a_movie
+		random = rand(5)
+		self[random]
+	end
 end
